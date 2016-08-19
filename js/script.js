796b7334abaca07bedc992f273845a4aaf02b4f2
Brian Dutz <bgdutz@gmail.com>
2016-08-19 12:06:30 -0700

    clean up code, add styling to display numbers/words on seperate lines

--- js/script.js
+++ js/script.js
@@ -1,27 +1,26 @@
+//back end
+
 function count(inputNumber) {
   var counter = 1;
   var numHolder = []
-  for (i=0; i<inputNumber; i++) {
+  for (i = 0; i < inputNumber; i++) {
     numHolder [i] = counter + " "
     counter++;
   };
-    for (i=0; i<inputNumber; i++) {
-  if ((numHolder[i]%15===0)===true){
+    for (i = 0; i < inputNumber; i++) {
+  if ((numHolder[i] % 15 === 0)=== true){
     numHolder[i] = "ping-pong" + " "
-  } else if((numHolder[i]%5===0)===true){
+  } else if((numHolder[i] % 5 === 0)=== true){
     numHolder[i] = "pong" + " "
-  } else if((numHolder[i]%3===0)===true){
+  } else if((numHolder[i] % 3 === 0)=== true){
     numHolder[i] ="ping" + " "
   }
 };
-  return numHolder
+  var resultString = numHolder.toString()
+  return resultString.split(",")
 };
 
-
-
-
-
-
+//front end
 
 $(document).ready(function() {
     $("form#numberInput").submit(function(event){
