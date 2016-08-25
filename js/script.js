aaa384fe6f773d11772335f0c29be8f6a37fe745
Brian Dutz <bgdutz@gmail.com>
2016-08-25 12:51:00 -0700

    add revision

--- js/script.js
+++ js/script.js
@@ -1,23 +1,21 @@
 //back end
 
 function count(inputNumber) {
-  var counter = 1;
   var numHolder = []
-  for (i = 0; i < inputNumber; i++) {
-    numHolder [i] = counter + " "
-    counter++;
+  for (i = 1; i < inputNumber+1; i++) {
+    numHolder.push(i);
   };
   for (i = 0; i < inputNumber; i++) {
-    if ((numHolder[i] % 15 === 0)=== true){
-        numHolder[i] = "ping-pong" + " "
-    } else if((numHolder[i] % 5 === 0)=== true){
-        numHolder[i] = "pong" + " "
-    } else if((numHolder[i] % 3 === 0)=== true){
-      numHolder[i] ="ping" + " "
+    if ((numHolder[i] % 15 === 0)){
+        numHolder[i] = "ping-pong"
+    } else if((numHolder[i] % 5 === 0)){
+        numHolder[i] = "pong"
+    } else if((numHolder[i] % 3 === 0)){
+      numHolder[i] ="ping"
     }
    };
   var resultString = numHolder.toString()
-  return resultString.split(",")
+  return resultString
 };
 
 //front end
